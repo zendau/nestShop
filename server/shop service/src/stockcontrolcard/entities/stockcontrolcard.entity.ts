5455a887892162065aa2bdbeacbfd6ebc0e420aa
Alex <Alex>
2022-03-12 18:18:05 +0300

    Add provider, waybill, scc, storage, worker, workerRole, sale entities

--- server/shop service/src/stockcontrolcard/entities/stockcontrolcard.entity.ts
+++ server/shop service/src/stockcontrolcard/entities/stockcontrolcard.entity.ts
@@ -1,1 +1,36 @@
-export class Stockcontrolcard {}
+import { Merchandise } from './../../merchandise/entities/merchandise.entity';
+import { Waybill } from './../../waybill/entities/waybill.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+
+@Entity()
+export class Stockcontrolcard {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+
+  @Column()
+  salePrice: number;
+
+  @Column()
+  arrivedDate: Date;
+  
+  @Column()
+  issueDate: Date;
+
+  @Column()
+  place: string;
+
+  @OneToMany(() => Waybill, (Waybill) => Waybill.id)
+  waybillId: number;
+
+  @OneToMany(() => Merchandise, (Merchandise) => Merchandise.id)
+  MerchandiseId: number;
+
+//   @OneToMany(() => UserRole, (userRole) => userRole.user)
+//   storageId: number;
+
+//   @OneToMany(() => UserRole, (userRole) => userRole.user)
+//   saleId: number;
+}
